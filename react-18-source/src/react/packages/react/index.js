93787b9a83449a343dae3b11a4bf6466f3b38e35
wangxing <982953267@qq.com>
2023-01-16 14:23:14 +0800

    [FIX]:做一个标记位

--- react-18-source/src/react/packages/react/index.js
+++ react-18-source/src/react/packages/react/index.js
@@ -28,6 +28,7 @@ export type ElementConfig<C> = React$ElementConfig<C>;
 export type ElementRef<C> = React$ElementRef<C>;
 export type Config<Props, DefaultProps> = React$Config<Props, DefaultProps>;
 export type ChildrenArray<+T> = $ReadOnlyArray<ChildrenArray<T>> | T;
+window.console.log('本地调试React代码');
 
 // Export all exports so that they're available in tests.
 // We can't use export * from in Flow for some reason.
