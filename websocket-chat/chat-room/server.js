5e21af6023ac33edb7417d736d2da46739aa7d55
wangxing <xing.wang@xforwardai.com>
2023-08-15 18:02:44 +0800

    [FIX]:做了一系列欧化  t提示

--- websocket-chat/chat-room/server.js
+++ websocket-chat/chat-room/server.js
@@ -4,6 +4,11 @@ import { Server } from "socket.io";
 import path from "path";
 
 let SYS = "系统";
+const MESSAGE_TYPE = {
+  SYS: "SYS",
+  SL: "SL",
+  GL: "GL",
+};
 
 const __dirname = path.resolve();
 
@@ -15,14 +20,16 @@ const server = http.createServer(app);
 
 let io = new Server(server);
 
-function createMessage(content, user) {
-  return { user, content, createTiime: new Date() };
+function createMessage(content, user, type = MESSAGE_TYPE.GL) {
+  return { user, content, createTiime: new Date(), messageType: type };
 }
 
 let socketMap = {};
 
 // of 是命名空间
 io.of("/chat").on("connection", (socket) => {
+  socket.emit("allUser", Object.keys(socketMap));
+
   let userName; // 每个客户端都给一个名字
 
   socket.on("message", function (message) {
@@ -37,17 +44,39 @@ io.of("/chat").on("connection", (socket) => {
         let toContent = result[2];
         const toSocket = socketMap[toUser];
         toSocket &&
-          toSocket.emit("message", createMessage(toContent, userName));
+          toSocket.emit(
+            "message",
+            createMessage(
+              `[${userName}向您私聊]：${toContent}`,
+              userName,
+              MESSAGE_TYPE.SL
+            )
+          );
+        socket.emit(
+          "message",
+          createMessage(
+            `[您向${toUser}私聊]：${toContent}`,
+            userName,
+            MESSAGE_TYPE.SL
+          )
+        );
       } else {
         // 公聊
-        io.of("/chat").emit("message", createMessage(message, userName));
+        io.of("/chat").emit(
+          "message",
+          createMessage(message, userName, MESSAGE_TYPE.GL)
+        );
       }
     } else {
       let oldSocket = socketMap[message];
       if (oldSocket) {
         socket.emit(
           "message",
-          createMessage(`${userName} 被占用，请使用其他名字！！！ `, SYS)
+          createMessage(
+            `${message} 被占用，请使用其他名字！！！ `,
+            SYS,
+            MESSAGE_TYPE.SYS
+          )
         );
         return;
       }
@@ -58,8 +87,28 @@ io.of("/chat").on("connection", (socket) => {
 
       socket.broadcast.emit(
         "message",
-        createMessage(`${userName} 加入房间！！！ `, SYS)
+        createMessage(`${userName} 加入房间！！！ `, SYS, MESSAGE_TYPE.SYS)
       );
+      io.of("/chat").emit("join", userName);
+    }
+  });
+  socket.on("leave", function () {
+    socket.broadcast.emit("leave", userName);
+    socket.broadcast.emit(
+      "message",
+      createMessage(`${userName} 离开房间！！！ `, SYS, MESSAGE_TYPE.SYS)
+    );
+  });
+
+  socket.on("disconnect", function (e) {
+    if (userName) {
+      socket.broadcast.emit("leave", userName);
+      socket.broadcast.emit(
+        "message",
+        createMessage(`${userName} 离开房间！！！ `, SYS, MESSAGE_TYPE.SYS)
+      );
+      socketMap[userName] = null;
+      delete socketMap[userName];
     }
   });
 });
